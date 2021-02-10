7df4cd10581c78377e559ceb64ddc06cdfc2e2bb
Dmytro Doroshchuk <dimariouz.dev@gmail.com>
2021-02-10 10:50:31 +0200

    create project

---
+++ SimpleAlbumList/SimpleAlbumList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpleAlbumList
+//
+//  Created by mac on 10.02.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
